f6bf36f2070a3d22a66309b716b7c7e087d36df0
valentin-panov <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>
2022-11-06 22:14:31 +0100

    feat(pages/404.tsx) fixed behaviour

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,7 +8,8 @@ export default function Error404() {
       <h2 className={styles.error}>Error 404</h2>
       <p className={styles.description}>
         Please{" "}
-        <Link href={`${process.env.HOST_URL}`} className={styles.link}>
+        {/*<Link href={`${process.env.HOST_URL}`} className={styles.link}>*/}
+        <Link href={`/`} className={styles.link}>
           go back
         </Link>{" "}
         to safety
