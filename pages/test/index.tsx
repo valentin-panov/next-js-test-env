c453381cb35afbe28851ae65f13aa961cbb2298f
Valentin Panov <valentin.panov@me.com>
2025-07-04 13:51:00 +0200

    credentialless

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -13,17 +13,24 @@ export default function Rest() {
   return (
     <MainLayout>
       <h2>REST</h2>
-      <div className={styles.grid}>
-        <div className={styles.card}>
-          <ClassicInput
-            value={inputString}
-            onChangeHandler={onChangeInputHandler}
-          />
-        </div>
+      <div  style={{width: '100%', display: 'flex',alignItems: 'stretch', justifyContent: 'space-evenly'}}>
+        <iframe
+            src="https://bug-collection.vercel.app/"
+            height="500px"
+            width="40%"
+            credentialless="true"
+        ></iframe>
+        <iframe src="https://bug-collection.vercel.app/" width="40%" height="500px"></iframe>
+        {/*<div className={styles.card}>*/}
+        {/*  <ClassicInput*/}
+        {/*    value={inputString}*/}
+        {/*    onChangeHandler={onChangeInputHandler}*/}
+        {/*  />*/}
+        {/*</div>*/}
 
-        <div className={styles.card}>
-          <Link href={inputString}>{inputString}</Link>
-        </div>
+        {/*<div className={styles.card}>*/}
+        {/*  <Link href={inputString}>{inputString}</Link>*/}
+        {/*</div>*/}
       </div>
     </MainLayout>
   );
